bcf8584dd8529d359161746e9906d877d461b846
Raghunandhanan <raghunandhanan.m@gmail.comgit config --global user.email raghunandhanan.n@gmail.com>
2021-06-24 02:18:02 +0530

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import netlifyIdentity from "netlify-identity-widget"
 
+if (netlifyIdentity) {
+  netlifyIdentity.on("init", user => {
+    if (!user) {
+      netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
 export default function Home() {
   return <div>Hello world!</div>
 }
